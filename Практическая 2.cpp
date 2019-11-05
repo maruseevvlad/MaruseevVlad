0439b1c101e60b4c9f8e9170dff2f72ad158c580
maruseevvlad <57216504+maruseevvlad@users.noreply.github.com>
2019-11-05 16:48:48 +0300

    Add files via upload

---
+++ Практическая 2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, y, x;
+	double e = 2.71;
+	cout << "Введите числа (a, b, x, y) : ";
+	cin >> a >> b >> x >> y;
+
+	if (x <= 0 || x < b)
+		cout<<"Ответ: " << a* (x * x) + b * (y * y) + log10(abs((a / b) * pow(pow(sin(x), 2) + pow(y, 3), 1 / 3)));
+	else if (x <= b && x < a)
+		cout << "Ответ: " << (pow(e, a + 1)) / tan(b * x);
+	else if (x >= a)
+		cout << "Ответ: " << pow((y - x), 2) + 2 / 3 * log10(abs(cos(a * x - b * y)));
+	
+
+	
+
+	}
+	
+
+
+
+
